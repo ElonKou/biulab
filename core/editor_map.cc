8bdb0c0214ef7ae6e755151ddcfc76390d3cf266
elonkou <elonkou@ktime.cc>
2019-09-27 07:49:15 +0800

    Draw line bugs version.

--- core/editor_map.cc
+++ core/editor_map.cc
@@ -8,6 +8,7 @@
 
 #include "editor_map.hh"
 #include <iostream>
+#include "cmath"
 #include "core.hh"
 
 using namespace std;
@@ -54,18 +55,19 @@ void MapEditor::drawLine(Map &mp, vec_2i pos_x, vec_2i pos_y) {
         }
     } else {
         float rate = (pos_y.y - pos_x.y) * 1.0 / (pos_y.x - pos_x.x);
-        if (rate == -1) {
-            for (int i = min_x; i <= max_x; i++) {
-                checkAndSetElem(mp, vec_2i(i, i * -1));
+        cout << "rate:" << rate << endl;
+        if (fabs(rate + 1) < 1.0e-6) {
+            for (int i = 0; i <= max_x - min_x; i++) {
+                checkAndSetElem(mp, vec_2i(min_x + i, max_y - i));
+                // cout << "VEC" << min_x + i << "," << min_y + i << endl;
             }
-        } else if (rate == 1) {
-            int min_x = min(pos_x.x, pos_y.x);
-            int max_x = max(pos_x.x, pos_y.x);
-            for (int i = min_x; i <= max_x; i++) {
-                checkAndSetElem(mp, vec_2i(i, i));
+        } else if (fabs(rate - 1) < 1.0e-6) {
+            for (int i = 0; i <= max_x - min_x; i++) {
+                checkAndSetElem(mp, vec_2i(min_x + i, min_y + i));
+                // cout << "HORI" << min_x + i << "," << min_y + i << endl;
             }
-        }
-        // else if (rate > 1 || rate < -1) {
+        } else 
+        // if (rate > 1 || rate < -1) {
         //     int temp = pos_x.x;
         //     pos_x.x = pos_x.y;
         //     pos_x.y = temp;
@@ -75,22 +77,27 @@ void MapEditor::drawLine(Map &mp, vec_2i pos_x, vec_2i pos_y) {
         // }
         // swap the pos and do check.
         if (rate > -1 && rate < 1) {
-            cout << "rate:" << rate << endl;
+            cout << "+++" << endl;
             int dx = abs(pos_y.x - pos_x.x);
             int dy = abs(pos_y.y - pos_x.y);
             int p = 2 * dy - dx;
             int twoDy = 2 * dy;
             int twoDyMinusDx = 2 * (dy - dx);
+            if(rate < 0){
+                twoDy *= -1;
+                // twoDyMinusDx *= -1;
+            }
             int x, y;
             if (pos_x.x > pos_y.x) {
                 x = pos_y.x;
                 y = pos_y.y;
+                pos_y.x = pos_x.x;
             } else {
                 x = pos_x.x;
                 y = pos_x.y;
             }
             checkAndSetElem(mp, vec_2i(x, y));
-            while (x < pos_x.y) {
+            while (x < pos_y.x) {
                 x++;
                 if (p < 0) {
                     p += twoDy;
